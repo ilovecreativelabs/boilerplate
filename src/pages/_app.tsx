298f27f6b72a7f2d38adcbad80b31fa5085ccc4b
Fernando dos Santos <nando4803@gmail.com>
2021-03-30 15:23:36 +0100

    feat: add styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Creative Labs</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#161616" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
