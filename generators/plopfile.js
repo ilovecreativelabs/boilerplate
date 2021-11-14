c299357a01ba76ced9e849f0fd2fa30f40c0d57a
Fernando dos Santos <nando4803@gmail.com>
2021-11-14 13:46:24 +0100

    feat: update boilerplate

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -19,16 +19,29 @@ module.exports = (plop) => {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
+      }
+    ]
+  })
+
+  plop.setGenerator('template', {
+    description: 'Create a template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your template name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
       }
     ]
   })
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
@@ -15,8 +19,9 @@ function App({ Component, pageProps }: AppProps) {
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
+      <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
