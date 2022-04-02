8b27792e3024b8746066932ac50f80f559e8ac90
Fernando dos Santos <nando4803@gmail.com>
2022-04-02 07:58:03 +0100

    feat: add TextField component

---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,107 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { TextFieldProps } from '.'
+
+type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
+
+type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean }
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    align-items: center;
+    background: #f8f8f8;
+    border: 0.2rem solid;
+    border-color: #f8f8f8;
+    border-radius: 0.2rem;
+    display: flex;
+    padding: 0 ${theme.spacings.xsmall};
+
+    &:focus-within {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Input = styled.input<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    background: transparent;
+    border: 0;
+    color: ${theme.colors.dark};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xsmall} 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
+    outline: none;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none;
+
+      &::first-line {
+        font-family: ${theme.font.family};
+        font-size: ${theme.font.sizes.medium};
+      }
+    }
+  `}
+`
+
+export const Label = styled.label`
+  ${({ theme }) => css`
+    color: ${theme.colors.dark};
+    cursor: pointer;
+    font-size: ${theme.font.sizes.small};
+  `}
+`
+
+export const Icon = styled.div<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    color: ${theme.colors.gray};
+    display: flex;
+    order: ${iconPosition === 'right' ? 1 : 0};
+
+    & > svg {
+      height: 100%;
+      width: 2.2rem;
+    }
+  `}
+`
+
+export const Error = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
+`
+
+const wrapperModifiers = {
+  error: (theme: DefaultTheme) => css`
+    ${InputWrapper} {
+      border-color: ${theme.colors.primary};
+    }
+    ${Icon},
+    ${Label} {
+      color: ${theme.colors.primary};
+    }
+  `,
+  disabled: (theme: DefaultTheme) => css`
+    ${Label},
+    ${Input},
+    ${Icon} {
+      color: ${theme.colors.gray};
+      cursor: not-allowed;
+
+      &::placeholder {
+        color: currentColor;
+      }
+    }
+  `
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, error, disabled }) => css`
+    ${error && wrapperModifiers.error(theme)}
+    ${disabled && wrapperModifiers.disabled(theme)}
+  `}
+`
