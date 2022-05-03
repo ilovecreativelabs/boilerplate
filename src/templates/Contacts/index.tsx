77a167befa5abf01ef5f3d32c81009e78cc63d2c
Fernando dos Santos <nando4803@gmail.com>
2022-05-03 10:23:31 +0100

    feat: add Contact page

---
+++ src/templates/Contacts/index.tsx
@@ -0,0 +1,13 @@
+import { Base } from 'templates'
+
+import * as S from './styles'
+
+const Contacts = () => (
+  <Base>
+    <S.Wrapper>
+      <h1>Contacts</h1>
+    </S.Wrapper>
+  </Base>
+)
+
+export default Contacts
